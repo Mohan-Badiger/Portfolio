fd3ccece865ebc323b9694cb6b28454bc4f02792
Mohan-Badiger <mohanbadiger250@gmail.com>
2025-12-04 12:00:15 +0530

    Update Work component layout for improved responsiveness

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,12 +26,12 @@ export default function Work() {
         // }
     ];
     return (
-        <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+        <div id="work" className="w-full px-[16%] sm:px-[12%] py-10 scroll-mt-20">
             <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
             <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
             <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">I have experience building responsive websites using HTML, CSS, JavaScript, and React.js. I worked on creating clean designs, interactive components.</p>
 
-            <div className="grid grid-cols-4 my-10 gap-5 dark:text-black">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4  my-10 gap-5 dark:text-black">
                 {work.map((work) => (
                     <div key={work.name} className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group" style={{ backgroundImage: `url(${work.icon})` }}>
                         <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
